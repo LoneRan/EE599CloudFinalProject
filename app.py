224fcfc33bff64a6d8c8d2a38e9110b1b0771cf8
Zeyu <wzy0766@gmail.com>
2021-04-29 17:21:23 +0800

    Zeyu

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+import configparser
 import requests
-import json
+
 app = Flask(__name__)
-CHECK = 0
+app.debug = True
 
+@app.route('/')
+def weather_dashboard():
+    return render_template('home.html')
 
-@app.route("/")
-def home():
-
-    return render_template("index.html")
-
-
-@app.route('/about')
-def form():
+@app.route('/results', methods=['POST'])
+def render_results():
+    zip_code = request.form['zipCode']
     
-    return render_template('about.html')
-
-@app.route('/contact')
-def data():
+    return "Zip Code: " + zip_code
     
-    return render_template('data.html')
+def get_api_key():
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+    return config['openweathermap']['api']
+
+def get_weather_results(zip_code, api_key):
+	 api_url = "https://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
+    r = requests.get(api_url)
+    return r.json()
+    
+if __name__ == '__main__':
+	app.run()
+
+
+@app.route('/results', methods=['POST'])
+def render_results():
+    zip_code = request.form['zipCode']
+
+    api_key = get_api_key()
+    data = get_weather_results(zip_code, api_key)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('results.html',
+                           location=location, temp=temp,
+                           feels_like=feels_like, weather=weather)
